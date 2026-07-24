586ded7e3d5fd204f9dd51f0202743a788f7cd4a
mysticdicc <richardfosterwork@gmail.com>
2026-07-24 01:06:28 +0100

    fixed config file import

--- cloudflare-dns-container/Program.cs
+++ cloudflare-dns-container/Program.cs
@@ -1,7 +1,12 @@
 using cloudflare_dns_container;
+using cloudflare_dns_container.Models;
 
-var builder = Host.CreateApplicationBuilder(args);
-builder.Services.AddHostedService<Worker>();
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<CloudflareDnsSettings>(context.Configuration.GetSection("CloudflareDnsSettings"));
+        services.AddHostedService<Worker>();
+    });
 
 var host = builder.Build();
 host.Run();
